be1a023a58a2905f244bd8893d1cbee959face53
Chris <chrismjboyle@gmail.com>
2019-11-28 16:29:11 +0000

    Run bot after connect

--- src/main.cpp
+++ src/main.cpp
@@ -8,10 +8,7 @@ int main(int argc, char const *argv[])
 {
   Robot bot = Robot("botpp", "testing");
   bot.connect(6667, "130.159.42.114");
-  // while (true) {
-  //   std::string reply = socket.read_line();
-  //   std::cout << reply << std::endl;
-  // }
+  bot.run();
   return 0;
 }
 
