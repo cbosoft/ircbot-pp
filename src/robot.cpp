fc9c14d351ce9eb571cbeb692934272cc2875e0c
Chris <chrismjboyle@gmail.com>
2019-11-28 12:53:34 +0000

    Connects to channel

--- src/robot.cpp
+++ src/robot.cpp
@@ -1,3 +1,5 @@
+#include <regex>
+
 #include "robot.hpp"
 #include "socket.hpp"
 #include "exception.hpp"
@@ -12,34 +14,94 @@ Robot::Robot(std::string nickname, std::string channel) {
 
 void Robot::connect(int port, std::string address) 
 {
-  socket = Socket(port, address);
-
-  try {
-    while (true) {
-      std::cout << socket.readline() << std::endl;
-    }
-  }
-  catch (Exception ex) {
-      ex.print();
-  }
+  this->socket = Socket(port, address);
 
   std::stringstream connect_msg_builder;
   connect_msg_builder << "NICK " << nickname << std::endl;
-  socket.write(connect_msg_builder.str());
+  this->socket.write(connect_msg_builder.str());
 
   connect_msg_builder.str("");
   connect_msg_builder << "USER " << nickname << " * 0 :bot"<< std::endl;
   socket.write(connect_msg_builder.str());
 
+  std::regex ping("PING(.*)");
+
+  std::cout << "Waiting for end of MOTD message indicating connection complete." << std::endl;
+  while (true) {
+
+    try {
+      std::string received = this->socket.readline();
+
+      if (received.find(":End of /MOTD") != std::string::npos) {
+        break;
+      }
+      else if (received.find("ERROR") != std::string::npos) {
+        exit(1);
+      }
+      else if (received.find("PING") != std::string::npos) {
+        std::string pong = std::string(received);
+        pong.replace(1, 1, "O");
+        this->socket.write(pong);
+      }
+    }
+    catch (TimeoutException ex) {
+      ex.print();
+      exit(1);
+    }
+
+  }
+
+  std::stringstream ss;
+  ss << "JOIN #" << this->channel << std::endl;
+  this->socket.write(ss.str());
+
+  std::cout << "Joining channel #" << this->channel << std::endl;
+  while (true) {
+
+    try {
+      std::string received = this->socket.readline();
+
+      if (received.find(":End of /NAMES") != std::string::npos) {
+        break;
+      }
+      else if (received.find("ERROR") != std::string::npos) {
+        exit(1);
+      }
+      else if (received.find("PING") != std::string::npos) {
+        std::string pong = std::string(received);
+        pong.replace(1, 1, "O");
+        this->socket.write(pong);
+      }
+    }
+    catch (TimeoutException ex) {
+      ex.print();
+      exit(1);
+    }
+
+  }
+}
+
+void Robot::run()
+{
   while (true) {
 
     try {
       std::string received = socket.readline();
-      std::cout << received << std::endl;
+      this->handle_input(received);
+
+    }
+    catch (TimeoutException ex) {
+      // ignore timeout stuff
     }
     catch (Exception ex) {
+      // other exceptions: print but don't exit
       ex.print();
     }
 
   }
+}
+
+void Robot::handle_input(std::string input)
+{
+  // TODO
 }
