17446939ead70987e8f94b794f989a8454c27e89
Chris <chrismjboyle@gmail.com>
2019-12-16 11:52:31 +0000

    Added command line flag "--testing" to join channel testing

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,18 @@
 
 int main(int argc, char const *argv[])
 {
-  Robot bot = Robot("botpp", "testing");
+  std::string channel = "general";
+
+  for (int i = 1; i < argc; i++) {
+    if (strcmp(argv[i], "--testing") == 0) {
+      channel = "testing";
+    }
+    else {
+      std::cout << "unrecognised argument: " << argv[i] << std::endl;
+    }
+  }
+
+  Robot bot = Robot("botpp", channel);
   bot.connect(6667, "130.159.42.114");
   bot.run();
   return 0;
